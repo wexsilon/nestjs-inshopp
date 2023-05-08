0764d9d6ea61ed346611e3fbce6eef350dbb962c
wexsilon <wexsilon2001@protonmail.com>
2023-05-08 22:10:45 +0430

    setup api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
 
     app.disable('x-powered-by');
+
+    const documentBuilder = new DocumentBuilder()
+        .setTitle('InShopP Api')
+        .setDescription('The InShopP API description')
+        .setVersion('1.0')
+        .addTag('InShopP')
+        .build();
+    const document = SwaggerModule.createDocument(app, documentBuilder);
+    SwaggerModule.setup('api', app, document);
+
 
     await app.listen(
         configService.get<number>('SERVER_PORT'),

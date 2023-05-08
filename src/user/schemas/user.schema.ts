49032d4ae73b5649281f12ef1e3bd1ff185342d2
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 00:07:03 +0430

    implement user schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+
+    @Prop({ type: SchemaTypes.String, required: true })
+    username: string;
+
+    @Prop({ type: SchemaTypes.String, required: true })
+    email: string;
+
+    @Prop({ type: SchemaTypes.String, required: true })
+    password: string;
+
+    @Prop({ type: SchemaTypes.Boolean, default: false })
+    verified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

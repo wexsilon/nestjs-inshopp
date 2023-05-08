5cb31afff7b58d121cda86a26a0610895661a250
wexsilon <wexsilon2001@protonmail.com>
2023-05-08 16:43:09 +0430

    import MongooseModule and connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [ConfigModule.forRoot({ isGlobal: true }),         MongooseModule.forRootAsync({
+        useFactory: async (
+            config: ConfigService,
+        ): Promise<MongooseModuleOptions> => {
+            let userPass = '';
+            if (config.get('DB_USER') && config.get('DB_PASS'))
+                userPass = `${config.get('DB_USER')}:${config.get(
+                    'DB_PASS',
+                )}@`;
+
+            return {
+                uri: `mongodb://${config.get('DB_HOST')}:${config.get(
+                    'DB_PORT',
+                )}/${config.get('DB_NAME')}`,
+            };
+        },
+        inject: [ConfigService],
+    })],
     controllers: [AppController],
     providers: [AppService],
 })

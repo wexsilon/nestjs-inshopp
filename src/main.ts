3148687762a4b16848a85ba3827e092ba1f4fa0b
wexsilon <wexsilon2001@protonmail.com>
2023-05-08 23:28:58 +0430

    change import of compression

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,9 @@ import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import * as compression from 'compression';
+import compression from 'compression';
 
 import { AppModule } from './app.module';
-
-
-
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);

cf8eb675756b6d518d272e2ae98fc64101fd2945
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 00:22:24 +0430

    implement CreateUserDto

---
+++ src/user/dtos/create-user.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsStrongPassword, Matches } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty({
+        example: 'thread8th',
+        name: 'username',
+        nullable: false,
+        required: true,
+    })
+    @Matches(RegExp('^[a-zA-Z][a-zA-Z0-9_]{8,}$'), {
+        message:
+            'name most greater than 7 and start with alphabet and just contain number or alphabet',
+    })
+    username: string;
+
+    @ApiProperty({
+        example: 'thread8@gmail.com',
+        name: 'email',
+        nullable: false,
+        required: true,
+    })
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({
+        example: '1234ThrEAd8@',
+        name: 'password',
+        nullable: false,
+        required: true,
+    })
+    @IsStrongPassword()
+    password: string;
+}

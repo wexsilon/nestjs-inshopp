0fca81c37d8691f497ec7dca4e11d075ff32f1bf
wexsilon <wexsilon2001@protonmail.com>
2023-05-08 16:43:14 +0430

    prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,28 @@ import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }),         MongooseModule.forRootAsync({
-        useFactory: async (
-            config: ConfigService,
-        ): Promise<MongooseModuleOptions> => {
-            let userPass = '';
-            if (config.get('DB_USER') && config.get('DB_PASS'))
-                userPass = `${config.get('DB_USER')}:${config.get(
-                    'DB_PASS',
-                )}@`;
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            useFactory: async (
+                config: ConfigService,
+            ): Promise<MongooseModuleOptions> => {
+                let userPass = '';
+                if (config.get('DB_USER') && config.get('DB_PASS'))
+                    userPass = `${config.get('DB_USER')}:${config.get(
+                        'DB_PASS',
+                    )}@`;
 
-            return {
-                uri: `mongodb://${config.get('DB_HOST')}:${config.get(
-                    'DB_PORT',
-                )}/${config.get('DB_NAME')}`,
-            };
-        },
-        inject: [ConfigService],
-    })],
+                return {
+                    uri: `mongodb://${config.get('DB_HOST')}:${config.get(
+                        'DB_PORT',
+                    )}/${config.get('DB_NAME')}`,
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })

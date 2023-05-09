254f0b5f7ec99b99f2e8932f3d87424f595bfb7d
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 16:48:30 +0430

    define StatusInterceptor

---
+++ src/common/interceptors/status.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { Response } from "express";
+
+
+export class StatusInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        return;
+    }
+
+}

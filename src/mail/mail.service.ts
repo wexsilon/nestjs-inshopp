dd3df4ad65090af69d140a8661dd5fe6de2bacb8
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 23:17:13 +0430

    implement sendVerifyEmail and use DI in constructor

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,25 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+
+    constructor(private readonly mailerService: MailerService, private readonly config: ConfigService,) {}
+
+    async sendVerifyEmail(email: string) {
+        await this.mailerService.sendMail({
+            to: email,
+            subject: 'Verify Email',
+            template: 'verify-email',
+            date: new Date(),
+            context: {
+                url: `http://${this.config.get(
+                    'SERVER_HOST',
+                )}:${this.config.get('SERVER_PORT')}/auth/verify/${
+                    '' //emailVerify.emailToken
+                }`,
+            },
+        });
+    }
+}

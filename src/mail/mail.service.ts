c5188692dac470a3d53104b11cae24e9b915e531
wexsilon <wexsilon2001@protonmail.com>
2023-05-10 00:29:41 +0430

    inject EmailVerify to MailService

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,12 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { EmailVerify, EmailVerifyDocument } from './schemas/email.verify.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MailService {
     constructor(
         private readonly mailerService: MailerService,
         private readonly config: ConfigService,
+        @InjectModel(EmailVerify.name) private readonly emailVerifyModel: Model<EmailVerifyDocument>
     ) {}
 
     async sendVerifyEmail(email: string) {

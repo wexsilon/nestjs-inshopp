923a22205d5c0949cacd28d0e74ac21e592ea352
wexsilon <wexsilon2001@protonmail.com>
2023-05-10 01:56:48 +0430

    import MailService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        MailService
     ],
     providers: [UserService],
     controllers: [UserController],

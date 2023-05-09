b620282dbc18fbbdf0ce0056b307bf39c1abb205
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 16:56:08 +0430

    add authService to AuthController (DI)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,14 @@ import { ApiTags, ApiBody } from '@nestjs/swagger';
 
 import { SignInUserDto } from './dtos/signin-user.dto';
 import { SignUpUserDto } from './dtos/signup-user.dto';
+import { AuthService } from './auth.service';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
+
+    constructor(private readonly authService: AuthService) {}
+
     @Post('signup')
     @ApiBody({ type: SignUpUserDto, required: true })
     async signUp(@Body() signUpUserDto: SignUpUserDto) {}

6975b3d5e01d02573c3c15af5a18ba07bdfbe95c
wexsilon <wexsilon2001@protonmail.com>
2023-05-09 22:59:57 +0430

    setup node-mailer in mail module and export MailService

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,43 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { join } from 'path';
 
 @Module({
-  providers: [MailService]
+    imports: [
+        MailerModule.forRootAsync({
+            useFactory: async (
+                config: ConfigService,
+            ): Promise<MailerOptions> => {
+                return {
+                    transport: {
+                        host: config.get<string>('EMAIL_HOST'),
+                        port: config.get<number>('EMAIL_PORT'),
+                        auth: {
+                            user: config.get<string>('EMAIL_USER'),
+                            pass: config.get<string>('EMAIL_PASS'),
+                        },
+                        secure: false,
+                        socketTimeout: 120000,
+                    },
+                    defaults: {
+                        from: `"No Reply" <${config.get<string>(
+                            'EMAIL_USER',
+                        )}>`,
+                    },
+                    template: {
+                        adapter: new EjsAdapter(),
+                        dir: join(__dirname, '..', '..', '..', 'views', 'mail'),
+                        options: {},
+                    },
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
+  providers: [MailService],
+  exports: [MailService]
 })
 export class MailModule {}
